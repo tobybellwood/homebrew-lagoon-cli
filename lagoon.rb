15a831ed22c180b28412ad4280798f069547461e
goreleaserbot <goreleaser@carlosbecker.com>
2021-09-17 18:47:48 +1000

    Brew formula update for lagoon-cli version v0.11.9

--- lagoon.rb
+++ lagoon.rb
@@ -5,28 +5,28 @@
 class Lagoon < Formula
   desc "amazee.io's local development helper tool"
   homepage "https://github.com/uselagoon/lagoon-cli"
-  version "0.11.8"
+  version "0.11.9"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/tobybellwood/lagoon-cli/releases/download/v0.11.8/lagoon-cli-0.11.8-darwin-amd64.tar.gz"
-      sha256 "ddff32d9098fbc5783ca51c0c46c2170c40f539400c34fb065735020d45019c1"
+      url "https://github.com/tobybellwood/lagoon-cli/releases/download/v0.11.9/lagoon-cli-v0.11.9-darwin-amd64.tar.gz"
+      sha256 "e05f99434da166e2c4b67b3ed207807d987f977c0a4c684a10201cf4a7105f2d"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/tobybellwood/lagoon-cli/releases/download/v0.11.8/lagoon-cli-0.11.8-darwin-arm64.tar.gz"
-      sha256 "8308db8553fdb9ec4d0c0cbeed48e8b5c1a2f19f60df8ba54cd63ce457282d38"
+      url "https://github.com/tobybellwood/lagoon-cli/releases/download/v0.11.9/lagoon-cli-v0.11.9-darwin-arm64.tar.gz"
+      sha256 "1bfa3291bffb999424c631c0994e61c23a6e15e0d18e1de72a719704c4ba449f"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/tobybellwood/lagoon-cli/releases/download/v0.11.8/lagoon-cli-0.11.8-linux-amd64.tar.gz"
-      sha256 "9c41acb29b9456590ba81864d3ccd6e0b86008733194e43d98dddd079d9f8b6c"
+      url "https://github.com/tobybellwood/lagoon-cli/releases/download/v0.11.9/lagoon-cli-v0.11.9-linux-amd64.tar.gz"
+      sha256 "6fe3adf52248ab49df28f29a5d5cd408abebcd656e05f627fa9107f08a58d346"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/tobybellwood/lagoon-cli/releases/download/v0.11.8/lagoon-cli-0.11.8-linux-arm64.tar.gz"
-      sha256 "5fdb1928de06ed6f0c20e530a38fb98b25280fa677198d3c0cfebe4a292a0e9f"
+      url "https://github.com/tobybellwood/lagoon-cli/releases/download/v0.11.9/lagoon-cli-v0.11.9-linux-arm64.tar.gz"
+      sha256 "72a1e6a4f7f8d16de35d6197a284ac1c30d20d506b1354ceccdf4f954bf8975b"
     end
   end
 
